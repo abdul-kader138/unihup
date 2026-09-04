3884fb417206a2e7b5cb91bf66fbd5ce1f4c867c
Abdul Kader <abdul.kader@edu.unifi.it>
2026-09-04 23:48:06 +0200

    Working on ui adjustment

--- deploy.sh
+++ deploy.sh
@@ -110,6 +110,8 @@ fi
 "$PHP_BIN" artisan storage:link
 "$PHP_BIN" artisan optimize
 "$PHP_BIN" artisan queue:restart
+"$PHP_BIN" universities:merge-duplicates
+"$PHP_BIN" universities:merge-duplicates --apply
 
 # Installs/keeps the persistent queue worker current. queue:restart above
 # only signals an ALREADY-RUNNING worker to gracefully restart between
